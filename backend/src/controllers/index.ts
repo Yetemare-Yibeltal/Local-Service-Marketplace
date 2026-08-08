208cb4637d7522c282e19aa78922d8dec7a9fcd6
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-08-09 00:37:55 +0300

    Add controllers index with all 11 controller exports

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -22,30 +22,25 @@ export {
 } from "./auth.controller";
 
 // ============================================================
-// BOOKING CONTROLLER
+// USER CONTROLLER
 // ============================================================
 
 export {
-  createBookingController,
-  getBookingByIdController,
-  getBookingByNumberController,
-  getCustomerBookingList,
-  getProviderBookingList,
-  adminGetAllBookings,
-  updateBookingController,
-  updateBookingStatusController,
-  cancelBookingController,
-  confirmBookingController,
-  startBookingController,
-  completeBookingController,
-  getProviderStatsController,
-  getCustomerStatsController,
-  checkBookingExistsController,
-  checkCustomerBookingOwner,
-  checkProviderBookingOwner,
-  getCustomerBookingView,
-  getProviderBookingView,
-} from "./booking.controller";
+  getMyProfile,
+  updateMyProfile,
+  getUserById,
+  getUsers,
+  getUserStats,
+  createUser,
+  updateUser,
+  deactivateUserController,
+  activateUserController,
+  deleteUserPermanently,
+  checkEmail,
+  checkPhone,
+  getUserByEmailController,
+  getUserByPhoneController,
+} from "./user.controller";
 
 // ============================================================
 // PROVIDER CONTROLLER
@@ -76,17 +71,212 @@ export {
 } from "./provider.controller";
 
 // ============================================================
+// BOOKING CONTROLLER
+// ============================================================
+
+export {
+  createBookingController,
+  getBookingByIdController,
+  getBookingByNumberController,
+  getCustomerBookingList,
+  getProviderBookingList,
+  adminGetAllBookings,
+  updateBookingController,
+  updateBookingStatusController,
+  cancelBookingController,
+  confirmBookingController,
+  startBookingController,
+  completeBookingController,
+  getProviderStatsController,
+  getCustomerStatsController,
+  checkBookingExistsController,
+  checkCustomerBookingOwner,
+  checkProviderBookingOwner,
+  getCustomerBookingView,
+  getProviderBookingView,
+} from "./booking.controller";
+
+// ============================================================
+// ADMIN CONTROLLER
+// ============================================================
+
+export {
+  getAdminDashboard,
+  adminGetAllUsers,
+  adminGetUser,
+  adminUpdateUserController,
+  adminDeactivateUserController,
+  adminActivateUserController,
+  adminGetAllProviders,
+  adminGetProvider,
+  adminVerifyProviderController,
+  adminGetPendingProviders,
+  adminGetAllDisputes,
+  adminGetDispute,
+  adminResolveDisputeController,
+  adminAddDisputeMessageController,
+  adminGetSettings,
+  adminGetSetting,
+  adminUpdateSetting,
+  adminGetAuditLogs,
+  adminGetPlatformAnalytics,
+} from "./admin.controller";
+
+// ============================================================
+// REVIEW CONTROLLER
+// ============================================================
+
+export {
+  createReviewController,
+  getReviewByIdController,
+  getReviewByBookingIdController,
+  getProviderReviewsController,
+  getMyReviewsController,
+  adminGetAllReviews,
+  updateReviewController,
+  deleteReviewController,
+  verifyReviewController,
+  addReviewResponseController,
+  updateReviewResponseController,
+  deleteReviewResponseController,
+  getProviderRatingStatsController,
+  checkReviewExistsController,
+  checkBookingHasReviewController,
+} from "./review.controller";
+
+// ============================================================
+// CATEGORY CONTROLLER
+// ============================================================
+
+export {
+  createCategoryController,
+  getCategoryByIdController,
+  getCategoryBySlugController,
+  getCategoriesController,
+  getActiveCategoriesController,
+  getRootCategoriesController,
+  getChildCategoriesController,
+  getCategoryTreeController,
+  getCategoryBreadcrumbController,
+  getCategoryDescendantsController,
+  getSubCategoryTreeController,
+  updateCategoryController,
+  deleteCategoryController,
+  hardDeleteCategoryController,
+  bulkUpdateCategoryOrderController,
+  getCategoryStatsController,
+  getCategoriesWithCountController,
+  validateCategoryNameController,
+  validateCategorySlugController,
+  checkSlugExistsController,
+} from "./category.controller";
+
+// ============================================================
+// SEARCH CONTROLLER
+// ============================================================
+
+export {
+  searchProvidersController,
+  getNearbyProvidersController,
+  searchByCategoryController,
+  searchByCityController,
+  autocompleteController,
+  getSuggestionsController,
+  getPopularSearchesController,
+  getSearchFiltersController,
+  getFeaturedProvidersController,
+  getSearchHistoryController,
+  clearSearchHistoryController,
+  getSearchStatsController,
+} from "./search.controller";
+
+// ============================================================
+// NOTIFICATION CONTROLLER
+// ============================================================
+
+export {
+  sendNotificationController,
+  sendBulkNotificationController,
+  sendEmailNotificationController,
+  getNotificationByIdController,
+  getMyNotificationsController,
+  getMyUnreadNotificationsController,
+  adminGetAllNotificationsController,
+  markNotificationReadController,
+  markAllNotificationsReadController,
+  getUnreadCountController,
+  hasUnreadNotificationsController,
+  deleteNotificationController,
+  deleteAllMyNotificationsController,
+  cleanupOldNotificationsController,
+  getNotificationPreferencesController,
+  updateNotificationPreferencesController,
+  checkNotificationExistsController,
+} from "./notification.controller";
+
+// ============================================================
+// ANALYTICS CONTROLLER
+// ============================================================
+
+export {
+  getBookingAnalyticsController,
+  getRevenueAnalyticsController,
+  getProviderPerformanceController,
+  getCustomerBehaviorController,
+  getCategoryAnalyticsController,
+  getAnalyticsDashboardController,
+} from "./analytics.controller";
+
+// ============================================================
+// PAYMENT CONTROLLER
+// ============================================================
+
+export {
+  initiatePaymentController,
+  verifyPaymentController,
+  processRefundController,
+  getPaymentByIdController,
+  getPaymentByBookingIdController,
+  getCustomerPaymentsController,
+  getProviderPaymentsController,
+  getPaymentStatsController,
+  getProviderEarningsController,
+  savePaymentMethodController,
+  getPaymentMethodsController,
+  deletePaymentMethodController,
+  processPaymentWebhookController,
+  checkBookingPaymentExistsController,
+  checkPaymentExistsController,
+} from "./payment.controller";
+
+// ============================================================
 // ALL CONTROLLERS AGGREGATED
 // ============================================================
 
 import * as authController from "./auth.controller";
-import * as bookingController from "./booking.controller";
+import * as userController from "./user.controller";
 import * as providerController from "./provider.controller";
+import * as bookingController from "./booking.controller";
+import * as adminController from "./admin.controller";
+import * as reviewController from "./review.controller";
+import * as categoryController from "./category.controller";
+import * as searchController from "./search.controller";
+import * as notificationController from "./notification.controller";
+import * as analyticsController from "./analytics.controller";
+import * as paymentController from "./payment.controller";
 
 export const controllers = {
   auth: authController,
-  booking: bookingController,
+  user: userController,
   provider: providerController,
+  booking: bookingController,
+  admin: adminController,
+  review: reviewController,
+  category: categoryController,
+  search: searchController,
+  notification: notificationController,
+  analytics: analyticsController,
+  payment: paymentController,
 };
 
 // ============================================================
@@ -95,7 +285,15 @@ export const controllers = {
 
 export default {
   ...authController,
-  ...bookingController,
+  ...userController,
   ...providerController,
+  ...bookingController,
+  ...adminController,
+  ...reviewController,
+  ...categoryController,
+  ...searchController,
+  ...notificationController,
+  ...analyticsController,
+  ...paymentController,
   controllers,
 };
